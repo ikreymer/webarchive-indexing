45e4d756be7002d557d00471c4bc5dff9b8162df
Ilya Kreymer <ikreymer@gmail.com>
2015-03-07 00:10:21 +0000

    fixes for running indexwarcs

--- indexwarcs.py
+++ indexwarcs.py
@@ -1,5 +1,12 @@
+import boto
+import shutil
+
 from mrjob.job import MRJob
 from mrjob.protocol import RawValueProtocol
+
+from tempfile import TemporaryFile
+from pywb.warc.cdxindexer import write_cdx_index
+from gzip import GzipFile
 
 
 class IndexWARCJob(MRJob):
@@ -10,17 +17,19 @@ class IndexWARCJob(MRJob):
     JOBCONF = {'mapred.line.input.format.linespermap': 1}
 
     def mapper_init(self):
-        super(IndexWARCJob, self).mapper_init()
-
         self.conn = boto.connect_s3()
         self.warc_bucket = self.conn.lookup('aws-publicdatasets')
+        assert(self.warc_bucket)
 
-        self.cdx_bucket = self.conn.lookup('cc-cdx')
+        self.cdx_bucket = self.conn.lookup('warcwork')
+        assert(self.cdx_bucket)
+
         self.cdx_path = '/cdx/dec2014/'
 
         self.index_options = {
+            'surt_ordered': True,
             'sort': True,
-            'append_post': True,
+#            'append_post': True,
             'cdx06': True,
             'minimal': True
         }
@@ -29,17 +38,31 @@ class IndexWARCJob(MRJob):
         filepath = line.split('\t')[-1]
         try:
             self._load_and_index(filepath)
-        except:
-            yield '', filepath
+        except Exception as exc:
+            import traceback
+            err_details = filepath + '\n'
+            err_details += traceback.format_exc(exc)
+            yield '', err_details
 
     def _load_and_index(self, filepath):
         warckey = self.warc_bucket.get_key(filepath)
 
-        with TemporaryFile(mode='w+b') as cdxtemp:
-            # Index to temp
-            write_cdx_index(cdxtemp, warckey, filename, self.index_options)
+        with TemporaryFile(mode='w+b') as warctemp:
+            shutil.copyfileobj(warckey, warctemp)
+            warctemp.seek(0)
 
-            # Upload temp
-            path = self.cdx_path + filepath.rsplit('/')[-1]
-            cdxkey = self.cdx_bucket.new_key(path)
-            cdxkey.set_contents_from_file(cdxtemp, rewind=True)
+            with TemporaryFile(mode='w+b') as cdxtemp:
+                cdxfile = GzipFile(fileobj=cdxtemp, mode='w+b')
+
+                # Index to temp
+                filename = filepath.rsplit('/')[-1]
+                write_cdx_index(cdxfile, warctemp, filename, **self.index_options)
+
+                # Upload temp
+                path = self.cdx_path + filename.replace('.warc.gz', '.cdx.gz')
+                cdxkey = self.cdx_bucket.new_key(path)
+                cdxkey.set_contents_from_file(cdxtemp, rewind=True)
+
+
+if __name__ == "__main__":
+    IndexWARCJob.run()
