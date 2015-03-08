2d6c9850f1cf5ab6efe78b6c99f5d30fbd8303ac
Ilya Kreymer <ikreymer@gmail.com>
2015-03-08 00:41:16 +0000

    update indexwarcs for latest iteration add custom exception for DoRetryException for retrying s3 issues explicitly

--- indexwarcs.py
+++ indexwarcs.py
@@ -1,5 +1,6 @@
 import boto
 import shutil
+import sys
 
 from mrjob.job import MRJob
 from mrjob.protocol import RawValueProtocol
@@ -8,6 +9,8 @@ from tempfile import TemporaryFile
 from pywb.warc.cdxindexer import write_cdx_index
 from gzip import GzipFile
 
+#boto.config.add_section('Boto')
+#boto.config.set('Boto','http_socket_timeout', '400')
 
 class IndexWARCJob(MRJob):
     INPUT_PROTOCOL = RawValueProtocol
@@ -21,10 +24,10 @@ class IndexWARCJob(MRJob):
         self.warc_bucket = self.conn.lookup('aws-publicdatasets')
         assert(self.warc_bucket)
 
-        self.cdx_bucket = self.conn.lookup('warcwork')
+        self.cdx_bucket = self.conn.lookup('cc-cdx-index')
         assert(self.cdx_bucket)
 
-        self.cdx_path = '/cdx/dec2014/'
+        #self.cdx_path = '/cdx/'
 
         self.index_options = {
             'surt_ordered': True,
@@ -35,32 +38,45 @@ class IndexWARCJob(MRJob):
         }
 
     def mapper(self, _, line):
-        filepath = line.split('\t')[-1]
+        warc_path = line.split('\t')[-1]
         try:
-            self._load_and_index(filepath)
+            self._load_and_index(warc_path)
+        except DoRetryException as rt:
+            return 1
         except Exception as exc:
             import traceback
-            err_details = filepath + '\n'
+            err_details = warc_path + '\n'
             err_details += traceback.format_exc(exc)
             yield '', err_details
 
-    def _load_and_index(self, filepath):
-        warckey = self.warc_bucket.get_key(filepath)
+    def _load_and_index(self, warc_path):
+        warckey = self.warc_bucket.get_key(warc_path)
+
+        # set cdx path
+        cdx_path = warc_path.replace('common-crawl/crawl-data', '/cdx')
+        cdx_path = cdx_path.replace('.warc.gz', '.cdx.gz')
+        cdxkey = self.cdx_bucket.get_key(cdx_path)
+        # already exists, skipping
+        if cdxkey:
+            sys.stderr.write('Already Exists\n')
+            return
 
         with TemporaryFile(mode='w+b') as warctemp:
-            shutil.copyfileobj(warckey, warctemp)
+            try:
+                shutil.copyfileobj(warckey, warctemp)
+            except:
+                raise DoRetryException()
+
             warctemp.seek(0)
 
             with TemporaryFile(mode='w+b') as cdxtemp:
-                cdxfile = GzipFile(fileobj=cdxtemp, mode='w+b')
-
-                # Index to temp
-                filename = filepath.rsplit('/')[-1]
-                write_cdx_index(cdxfile, warctemp, filename, **self.index_options)
+                with GzipFile(fileobj=cdxtemp, mode='w+b') as cdxfile:
+                    # Index to temp
+                    write_cdx_index(cdxfile, warctemp, warc_path, **self.index_options)
 
                 # Upload temp
-                path = self.cdx_path + filename.replace('.warc.gz', '.cdx.gz')
-                cdxkey = self.cdx_bucket.new_key(path)
+                cdxkey = self.cdx_bucket.new_key(cdx_path)
+                cdxtemp.flush()
                 cdxkey.set_contents_from_file(cdxtemp, rewind=True)
 
 
