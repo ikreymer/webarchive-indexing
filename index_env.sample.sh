f079e8a32b9be35f30b99a46fc33652fd1747539
Ilya Kreymer <ikreymer@gmail.com>
2015-03-11 23:15:01 +0000

    add sample index_env.sample.sh

---
+++ index_env.sample.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+export AWS_ACCESS_KEY_ID="<s3 id>"
+export AWS_SECRET_ACCESS_KEY="<s3 secret>"
+
+export WARC_MANIFEST="<warc input file>"
+export WARC_CDX_BUCKET="<cdx output bucket>"
+
+export WARC_CDX="<per warc cdx directory>"
+
+export SPLIT_FILE="<shard split file location>"
+export ZIPNUM_CLUSTER_DIR="<final zipnum cluster directory>"
